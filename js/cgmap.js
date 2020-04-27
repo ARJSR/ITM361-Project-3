33f27c660c2f97f1f234e4febd2f7f9da92c03a6
Antonio Jackson, SR <ajackson7@hawk.iit.edu>
2020-04-27 17:32:27 -0500

    correcting erros

--- js/cgmap.js
+++ js/cgmap.js
@@ -1,6 +1,6 @@
   function init(){
     var element = document.getElementById('canvas');
-	var myLocation = new google.maps.LatLng(41.692266, -87.681491);
+	var myLocation = new google.maps.LatLng(41.692222, -87.681459);
 	var mapOptions = {
 	  center: myLocation,
 	  zoom: 18,
@@ -17,14 +17,16 @@
     map: map,
     draggable: true,
     animation: google.maps.Animation.DROP,
-    position: {lat: 41.692266, lng: -87.681491}
+    position: {lat: 41.692222, lng: -87.681459}
   });
+  
   marker.addListener('click', toggleBounce);
-}
-
-function toggleBounce() {
+  function toggleBounce() {
   if (marker.getAnimation() !== null) {
     marker.setAnimation(null);
   } else {
     marker.setAnimation(google.maps.Animation.BOUNCE);
   }
+  } 
+}
+ google.maps.event.addDomListener(windo, 'load', init);
